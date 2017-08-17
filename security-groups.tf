6e5cb38fd3db96cea8efb3b88fa0b02bfb71e273
huwenfeng <huwenfengredhat@163.com>
2017-08-17 15:55:47 +0800

    add tf files
    add .tf files and related dirs & files

---
+++ security-groups.tf
@@ -0,0 +1,89 @@
+/* Default security group */
+resource "aws_security_group" "default" {
+  name = "default-github-example"
+  description = "Default security group that allows inbound and outbound traffic from all instances in the VPC"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    self        = true
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    self        = true
+  }
+
+  tags { 
+    Name = "github-example-default-vpc" 
+  }
+}
+
+/* Security group for the nat server */
+resource "aws_security_group" "nat" {
+  name = "nat-github-example"
+  description = "Security group for nat instances that allows SSH and VPN traffic from internet. Also allows outbound HTTP[S]"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 1194
+    to_port   = 1194
+    protocol  = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags { 
+    Name = "nat-github-example" 
+  }
+}
+
+/* Security group for the web */
+resource "aws_security_group" "web" {
+  name = "web-github-example"
+  description = "Security group for web that allows web traffic from internet"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags { 
+    Name = "web-github-example" 
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "access_key" { 
+  description = "INPUT your AWS access_key"
+}
+
+variable "secret_key" { 
+  description = "INPUT your AWS secret_key"
+}
+
+variable "region"     { 
+  description = "AWS region to host your network"
+  default     = "us-east-1" 
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for VPC"
+  default     = "10.128.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for public subnet"
+  default     = "10.128.0.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR for private subnet"
+  default     = "10.128.1.0/24"
+}
+
+/* Ubuntu 14.04 amis by region */
+variable "amis" {
+  description = "Base AMI to launch the instances with"
+  default = {
+    us-west-1 = "ami-049d8641" 
+    us-east-1 = "ami-a6b8e7ce"
+  }
+}
